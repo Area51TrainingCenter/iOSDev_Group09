9078f0c3d365b70294130ad26c6afaaef6c0250c
frantisan <frantisaul@gmail.com>
2014-02-04 20:32:45 -0500

    DatoPasadoViewController e IBOutlets

---
+++ module_1/class_04/PasarDatos/PasarDatos/DatoPasadoViewController.h
@@ -0,0 +1,14 @@
+//
+//  DatoPasadoViewController.h
+//  PasarDatos
+//
+//  Created by Franti Saúl Huamán Mera on 2/4/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DatoPasadoViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *cadenaConcatenadaAMostrar;
+
+@end
--- module_1/class_04/PasarDatos/PasarDatos/ViewController.h
+++ module_1/class_04/PasarDatos/PasarDatos/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *cajaUno;
+@property (weak, nonatomic) IBOutlet UITextField *cajaDos;
 
 @end
