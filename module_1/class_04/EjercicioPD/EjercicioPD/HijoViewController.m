60862b4dc5b669e56ed87607acf0a95ca26850ae
frantisan <frantisaul@gmail.com>
2014-02-04 19:35:51 -0500

    Asignando HijoViewController class a la nueva vista

---
+++ module_1/class_04/EjercicioPD/EjercicioPD/HijoViewController.m
@@ -0,0 +1,38 @@
+//
+//  HijoViewController.m
+//  EjercicioPD
+//
+//  Created by Franti Saúl Huamán Mera on 2/4/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "HijoViewController.h"
+
+@interface HijoViewController ()
+
+@end
+
+@implementation HijoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
