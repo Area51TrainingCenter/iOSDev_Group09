4a32cbe5db121b7c7bc74c621c2b86ee902ed173
frantisan <frantisaul@gmail.com>
2014-02-04 20:19:04 -0500

    Initial Commit

---
+++ module_1/class_04/PasarDatos/PasarDatosTests/PasarDatosTests.m
@@ -0,0 +1,34 @@
+//
+//  PasarDatosTests.m
+//  PasarDatosTests
+//
+//  Created by Franti Saúl Huamán Mera on 2/4/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PasarDatosTests : XCTestCase
+
+@end
+
+@implementation PasarDatosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
