812997d92550bcdd10af4316b4c8e01bbab1a2c9
frantisan <frantisaul@gmail.com>
2014-02-04 21:40:49 -0500

    Initial Commit

---
+++ module_1/class_04/EjercicioPasarDato/EjercicioPasarDato/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EjercicioPasarDato
+//
+//  Created by Franti Saúl Huamán Mera on 2/4/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- module_1/class_04/EjercicioPasarDato/EjercicioPasarDato/ViewController.m
+++ module_1/class_04/EjercicioPasarDato/EjercicioPasarDato/ViewController.m
@@ -1,13 +1,12 @@
 //
 //  ViewController.m
-//  PasarDatos
+//  EjercicioPasarDato
 //
 //  Created by Franti Saúl Huamán Mera on 2/4/14.
 //  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
 //
 
 #import "ViewController.h"
-#import "DatoPasadoViewController.h"
 
 @interface ViewController ()
 
@@ -15,16 +14,16 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad{
+- (void)viewDidLoad
+{
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
-- (void)didReceiveMemoryWarning{
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    DatoPasadoViewController *o = segue.destinationViewController;
-    o.datoPasado = [NSString stringWithFormat:@"%@ %@",self.cajaUno.text, self.cajaDos.text];
-}
+
 @end
