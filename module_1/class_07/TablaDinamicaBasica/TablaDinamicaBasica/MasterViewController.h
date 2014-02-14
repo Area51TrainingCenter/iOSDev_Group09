9751a5842f048353e6603e684acf4cc24ced889a
frantisan <frantisaul@gmail.com>
2014-02-13 19:12:48 -0500

    Escenario inicial

---
+++ module_1/class_07/TablaDinamicaBasica/TablaDinamicaBasica/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  TablaDinamicaBasica
+//
+//  Created by Franti Saúl Huamán Mera on 2/13/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
