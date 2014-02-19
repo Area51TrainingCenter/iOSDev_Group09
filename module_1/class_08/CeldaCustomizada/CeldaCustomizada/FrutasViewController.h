467b0b09b4ad54486b88ceb4f85f510b576dc188
frantisan <frantisaul@gmail.com>
2014-02-18 20:36:31 -0500

    mostrando celdas en la tabla

---
+++ module_1/class_08/CeldaCustomizada/CeldaCustomizada/FrutasViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrutasViewController.h
+//  CeldaCustomizada
+//
+//  Created by Franti Saúl Huamán Mera on 2/18/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrutasViewController : UITableViewController
+
+@end
---
+++ module_1/class_08/CeldaCustomizada/CeldaCustomizada/FrutasViewController.m
@@ -0,0 +1,51 @@
+//
+//  FrutasViewController.m
+//  CeldaCustomizada
+//
+//  Created by Franti Saúl Huamán Mera on 2/18/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "FrutasViewController.h"
+
+@interface FrutasViewController ()
+
+@end
+
+@implementation FrutasViewController
+
+#pragma mark -
+#pragma mark LifeCycle Methods
+- (id)initWithStyle:(UITableViewStyle)style{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
+- (void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark - Table View Data Source Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 9;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"frutaCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
