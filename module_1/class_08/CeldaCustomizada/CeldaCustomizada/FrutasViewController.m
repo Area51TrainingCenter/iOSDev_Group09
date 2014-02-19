f63a1303af74d9efc32c9ede9c1df9244f7fb2c6
frantisan <frantisaul@gmail.com>
2014-02-18 21:33:49 -0500

    ejercicio terminado

--- module_1/class_08/CeldaCustomizada/CeldaCustomizada/FrutasViewController.m
+++ module_1/class_08/CeldaCustomizada/CeldaCustomizada/FrutasViewController.m
@@ -10,7 +10,7 @@
 #import "MiCeldaCell.h"
 
 @interface FrutasViewController ()
-
+@property (nonatomic, strong) NSArray *listaDefrutas;
 @end
 
 @implementation FrutasViewController
@@ -26,6 +26,19 @@
 }
 - (void)viewDidLoad{
     [super viewDidLoad];
+    [self creandoLaData];
+}
+- (void)creandoLaData{
+    NSDictionary *uno = [NSDictionary dictionaryWithObjectsAndKeys:@"Manzana", @"Nombre", @"12", @"Precio", nil];
+    NSDictionary *dos = [NSDictionary dictionaryWithObjectsAndKeys:@"Fresa", @"Nombre", @"10", @"Precio", nil];
+    NSDictionary *tres = [NSDictionary dictionaryWithObjectsAndKeys:@"Maracuya", @"Nombre", @"8", @"Precio", nil];
+    NSDictionary *cuatro = [NSDictionary dictionaryWithObjectsAndKeys:@"Uva", @"Nombre", @"15", @"Precio", nil];
+    NSDictionary *cinco = [NSDictionary dictionaryWithObjectsAndKeys:@"Naranja", @"Nombre", @"6", @"Precio", nil];
+    NSDictionary *seis = [NSDictionary dictionaryWithObjectsAndKeys:@"Mango", @"Nombre", @"16", @"Precio", nil];
+    NSDictionary *siete = [NSDictionary dictionaryWithObjectsAndKeys:@"Mandarina", @"Nombre", @"3", @"Precio", nil];
+    NSDictionary *ocho = [NSDictionary dictionaryWithObjectsAndKeys:@"Pera", @"Nombre", @"4", @"Precio", nil];
+    
+    self.listaDefrutas = [NSArray arrayWithObjects:uno, dos, tres, cuatro, cinco, seis, siete, ocho, nil];
 }
 - (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
@@ -36,16 +49,26 @@
 #pragma mark - Table View Data Source Methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     return 1;
+    //return [self.listaDefrutas count];
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 9;
+    return [self.listaDefrutas count];
+    //return 1;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     MiCeldaCell *cell = [tableView dequeueReusableCellWithIdentifier:@"frutaCell" forIndexPath:indexPath];
-    cell.nombreDefruta.text = @"Naranja";
-    cell.precioDefruta.text = @"12";
-    [cell.esCaro setOn:NO];
+    NSDictionary *bean = [self.listaDefrutas objectAtIndex:indexPath.row];
+    
+    cell.nombreDefruta.text = [bean objectForKey:@"Nombre"];
+    cell.precioDefruta.text = [bean objectForKey:@"Precio"];
+    
+    if ([[bean objectForKey:@"Precio"] integerValue]>10) {
+        [cell.esCaro setOn:YES];
+    }else{
+        [cell.esCaro setOn:NO];
+    }
+    
     
     // Configure the cell...
     
