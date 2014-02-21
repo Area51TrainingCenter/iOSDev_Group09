b67b625050c709534b5142665b2b12f0c3008de5
frantisan <frantisaul@gmail.com>
2014-02-20 19:27:43 -0500

    Escenario inicial

---
+++ module_2/class_01/RecordandoProtocoloDelegado/RecordandoProtocoloDelegado/PapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  PapaViewController.h
+//  RecordandoProtocoloDelegado
+//
+//  Created by Franti Saúl Huamán Mera on 2/20/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PapaViewController : UITableViewController
+
+@end
