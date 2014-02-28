3d6640c6b9dd4cf19cc77b7e9382c0f01d94f784
frantisan <frantisaul@gmail.com>
2014-02-27 20:12:08 -0500

    asignando clases a cada escena.

---
+++ module_2/class_03/EjercicioMVC/EjercicioMVC/App/View/ListaViewController.m
@@ -0,0 +1,46 @@
+//
+//  ListaViewController.m
+//  EjercicioMVC
+//
+//  Created by Franti Saúl Huamán Mera on 2/27/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "ListaViewController.h"
+
+@interface ListaViewController ()
+
+@end
+
+@implementation ListaViewController
+
+#pragma mark -
+#pragma mark LifeCycle Methods
+- (id)initWithStyle:(UITableViewStyle)style{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
+- (void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+#pragma mark - Table View Data Source Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 0;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 0;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"testeCell" forIndexPath:indexPath];
+    return cell;
+}
+@end
---
+++ module_2/class_03/EjercicioMVC/EjercicioMVC/App/View/NuevoViewController.m
@@ -0,0 +1,33 @@
+//
+//  NuevoViewController.m
+//  EjercicioMVC
+//
+//  Created by Franti Saúl Huamán Mera on 2/27/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "NuevoViewController.h"
+
+@interface NuevoViewController ()
+
+@end
+
+@implementation NuevoViewController
+
+#pragma mark -
+#pragma mark LifeCycle Methods
+- (id)initWithStyle:(UITableViewStyle)style{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (void)viewDidLoad{
+    [super viewDidLoad];
+}
+- (void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
