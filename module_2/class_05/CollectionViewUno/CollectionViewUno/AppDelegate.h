4ca45edffd5edc4bad67997edc960dab02538d7d
frantisan <frantisaul@gmail.com>
2014-03-06 19:40:23 -0500

    Initial Commit

---
+++ module_2/class_05/CollectionViewUno/CollectionViewUno/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewUno
+//
+//  Created by Franti Saúl Huamán Mera on 3/6/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
