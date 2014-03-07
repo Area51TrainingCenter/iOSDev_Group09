28ec5288966fc8cd1430c2cccc7ddbd1a1138f5e
frantisan <frantisaul@gmail.com>
2014-03-06 19:55:46 -0500

    Escenario inicial

---
+++ module_2/class_05/CollectionViewUno/CollectionViewUno/App/View/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  CollectionViewUno
+//
+//  Created by Franti Saúl Huamán Mera on 3/6/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UICollectionViewController
+
+@end
