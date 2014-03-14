0f4c2fdd996d75e4d876ec75490200d9ec820e7a
frantisan <frantisaul@gmail.com>
2014-03-13 19:41:33 -0500

    creación de entidad Usuario y su clase

---
+++ module_2/class_06/Login/Login/App/Model/Usuario.h
@@ -0,0 +1,19 @@
+//
+//  Usuario.h
+//  Login
+//
+//  Created by Franti Saúl Huamán Mera on 3/13/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Usuario : NSManagedObject
+
+@property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSString * nombre;
+@property (nonatomic, retain) NSString * clave;
+
+@end
---
+++ module_2/class_06/Login/Login/App/Model/Usuario.m
@@ -0,0 +1,18 @@
+//
+//  Usuario.m
+//  Login
+//
+//  Created by Franti Saúl Huamán Mera on 3/13/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "Usuario.h"
+
+
+@implementation Usuario
+
+@dynamic email;
+@dynamic nombre;
+@dynamic clave;
+
+@end
