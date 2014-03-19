77cb9d0ef3180e559a5567df25fc95161fe754c6
frantisan <frantisaul@gmail.com>
2014-03-18 19:42:44 -0500

    Initial Commit

---
+++ module_2/class_08/ParseLogin/ParseLoginTests/ParseLoginTests.m
@@ -0,0 +1,34 @@
+//
+//  ParseLoginTests.m
+//  ParseLoginTests
+//
+//  Created by Franti Saúl Huamán Mera on 3/18/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ParseLoginTests : XCTestCase
+
+@end
+
+@implementation ParseLoginTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
