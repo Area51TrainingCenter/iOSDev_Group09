787a855cfb9dd962100ff08cae6bd8e3136573ad
frantisan <frantisaul@gmail.com>
2014-03-18 19:13:12 -0500

    escenario inicial

---
+++ module_2/class_08/Views/Views/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Views
+//
+//  Created by Franti Saúl Huamán Mera on 3/18/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UIViewController
+
+@end
---
+++ module_2/class_08/Views/Views/MasterViewController.m
@@ -0,0 +1,35 @@
+//
+//  MasterViewController.m
+//  Views
+//
+//  Created by Franti Saúl Huamán Mera on 3/18/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+#pragma mark -
+#pragma mark LifeCycle Methods
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (void)viewDidLoad{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+- (void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
