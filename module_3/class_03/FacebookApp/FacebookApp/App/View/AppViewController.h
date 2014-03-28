e4775b42cd440e1f47620e84318bc52586ee678c
frantisan <frantisaul@gmail.com>
2014-03-27 20:20:03 -0500

    escenario inicial

---
+++ module_3/class_03/FacebookApp/FacebookApp/App/View/AppViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppViewController.h
+//  FacebookApp
+//
+//  Created by Franti Saúl Huamán Mera on 3/27/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppViewController : UIViewController
+
+@end
---
+++ module_3/class_03/FacebookApp/FacebookApp/App/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  FacebookApp
+//
+//  Created by Franti Saúl Huamán Mera on 3/27/14.
+//  Copyright (c) 2014 Franti Saúl Huamán Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
